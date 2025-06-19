8bea6077a9d5062433f3bbbcb5c385440bce903e
Marianooss <106757518+Marianooss@users.noreply.github.com>
2025-06-18 21:24:53 -0300

    Add .next/static/chunks/src_app_virtual-events_page_tsx_7554c5be._.js 1750292702416

---
+++ .next/static/chunks/src_app_virtual-events_page_tsx_7554c5be._.js
@@ -0,0 +1,7 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/_c2a9cced._.js"
+],
+    source: "dynamic"
+});
